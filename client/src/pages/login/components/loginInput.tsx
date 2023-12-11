089dffb290da1dbb88d6078f74445fb2dffb7085
Andres <andresrocabado99@gmail.com>
2023-12-11 15:31:05 -0400

    Haciendo login

--- client/src/pages/login/components/loginInput.tsx
+++ client/src/pages/login/components/loginInput.tsx
@@ -2,22 +2,25 @@ import { useId } from "react";
 
 type Props = {
   label: string,
-  type: string
+  type: string,
+  icon: JSX.Element
 }
 
-const LoginInput = ({label, type} : Props) => {
+const LoginInput = ({label, type, icon} : Props) => {
   const id = useId();
 
   return (
     <div className="flex gap-4 items-center">
-      <label htmlFor={id}>{label}</label>
-      <div className="flex h-8">
-        <div className="h-full aspect-square border border-solid border-gray-600 border-r-0"></div>
+      <div className="flex h-8 relative rounded-lg overflow-hidden items-center">
+        <label htmlFor="" className="absolute p-1 text-slate-400 text-md left-1">{label}</label>
         <input 
           id={id} 
           type={type} 
-          className="h-full border border-solid border-gray-600"
+          className="h-full "
         />
+        <div className="h-full aspect-square bg-white">
+          {icon}
+        </div>
       </div>
     </div>
   )
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,29 +1,9 @@
-import Grass from "../../assets/images/login/grass.jpg";
-import Logo from "../../assets/images/login/GoalGuardian.png";
-import QR from "../../assets/images/login/QR.png";
 import LoginForm from "./components/loginForm";
 
 const Index = () => {
   return (
-    <section className="w-screen h-screen flex">
-      <div className="h-full w-[30%]">
-        <img 
-          className="h-full object-cover"
-          src={Grass} 
-          alt="grass" 
-        />
-      </div>
-      <div className="h-full flex-1 bg-gray-200 relative flex items-center justify-center">
-        <div className="absolute top-8 left-8 flex items-center gap-4">
-          <img src={Logo} alt="logo" className="w-48 rounded-lg" />
-          <h1 className="font-semibold text-3xl">Goal Guardian</h1>
-        </div>
-        <LoginForm />
-        <div className="absolute bottom-8 left-8 flex flex-col items-center">
-          <img src={QR} alt="qr" className="w-36" />
-          <small>Copyright am2ps 2023</small>
-        </div>
-      </div>
+    <section className="w-screen h-screen flex justify-center items-center">
+      <LoginForm/>
     </section>
   )
 }
