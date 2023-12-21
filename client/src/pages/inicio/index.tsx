314464a2403e09ba95bb9f8cec8d3d28454f865e
MQInfer12 <maummq@gmail.com>
2023-12-20 20:49:35 -0400

    Comprar entradas

--- client/src/pages/inicio/index.tsx
+++ client/src/pages/inicio/index.tsx
@@ -4,23 +4,39 @@ import Loader from "../../global/components/loader/loader";
 import PageContainer from "../../global/components/pageContainer";
 import { Evento } from "../../global/interfaces/api/evento";
 import { useGet } from "../../hooks/useGet";
+import { useUser } from "../../store/user";
+import { Roles } from "../../global/interfaces/types/roles";
 
 const Inicio = () => {
   const navigate = useNavigate();
   const { res } = useGet<Evento[]>("EventType");
+  const { user } = useUser();
+
+  if (user?.roleName === Roles.cliente)
+    return (
+      <PageContainer title="Inicio">
+        {!res ? (
+          <Loader />
+        ) : (
+          res.data.map((evento) => (
+            <div key={evento.id}>
+              <p>{evento.typeEventName}</p>
+              <Button
+                onClick={() =>
+                  navigate(`/dashboard/inicio/verEvento/${evento.id}`)
+                }
+              >
+                Ver entradas
+              </Button>
+            </div>
+          ))
+        )}
+      </PageContainer>
+    );
 
   return (
     <PageContainer title="Inicio">
-      {!res ? (
-        <Loader />
-      ) : (
-        res.data.map((evento) => (
-          <div key={evento.id}>
-            <p>{evento.typeEventName}</p>
-            <Button onClick={() => navigate(`/dashboard/inicio/verEvento/${evento.id}`)}>Ver entradas</Button>
-          </div>
-        ))
-      )}
+      <p>Inicio</p>
     </PageContainer>
   );
 };
